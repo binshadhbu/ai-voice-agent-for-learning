a2608572e3121b501af1f57042140f60c4d60503
binshadhbu <binshadhubasheer@gmail.com>
2025-06-19 10:52:20 +0530

    imaplemented cta and list

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,26 @@
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
+import { recentSessions } from "@/constants";
 
 export default function Home() {
   return (
-   <div className="text-2xl underline">Home page</div>
+    <div>
+      <h1 className="home-section">Popular Companions</h1>
+      <section className="home-section">
+        <CompanionCard id="123" subject="science" name="the brainy explorer" topic="science" duration={45} color="#ffda6" />
+        <CompanionCard id="124" subject="history" name="the adventurous traveler" topic="history" duration={30} color="#e5d0ff" />
+        <CompanionCard id="125" subject="math" name="the curious learner" topic="math" duration={60} color="#ffda6e" />
+      </section>
+
+      <section className="home-section">
+        <CompanionsList title="Recently completed sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full" />
+          
+        <CTA />
+
+      </section>
+    </div>
   );
 }
